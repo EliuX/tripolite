06e95a4c67b1ed851423969c484f268b86249607
EliuX <eliecerhdz@gmail.com>
2024-07-06 02:22:59 -0400

    build(api): change express server port to 3001

--- backend/api/src/server.ts
+++ backend/api/src/server.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import {AppDataSource} from "./data-source";
 import * as process from "process";
 
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 3001;
 
 AppDataSource
     .initialize()
