883796e9a7c447823387fda6ab882da51b072227
EliuX <eliecerhdz@gmail.com>
2024-07-06 00:38:34 -0400

    feat(website): define structure and routes

---
+++ frontend/website/app/bookings/page.tsx
@@ -0,0 +1,12 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'TripOlite - Bookings',
+}
+export default function Page() {
+    return (
+        <>
+            Your current bookings
+        </>
+    );
+}
---
+++ frontend/website/app/travel-routes/page.tsx
@@ -0,0 +1,22 @@
+export default function Page() {
+    return (
+        <section>
+            <h1>All Travel Routes</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Origin City</th>
+                    <th>Destination City</th>
+                    <th>Transportation</th>
+                    <th>Type</th>
+                    <th>Price</th>
+                    <th>Schedule</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </section>
+    );
+}
