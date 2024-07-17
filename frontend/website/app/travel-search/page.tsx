1bde27d3225f885d1b7b4f32c8fbe1e5168af5e9
EliuX <eliecerhdz@gmail.com>
2024-07-17 12:05:31 -0400

    fix(website): go home when invalid search criteria

--- frontend/website/app/travel-search/page.tsx
+++ frontend/website/app/travel-search/page.tsx
@@ -3,16 +3,15 @@
 import * as React from "react";
 import {useEffect, useState} from "react";
 import {searchTravelsChoices} from "@/lib/api";
-import {selectTravelSearchCriteria} from "@/lib/selectors";
-import {useAppSelector} from "@/lib/hooks";
-import {router} from "next/client";
+import {isTravelSearchCriteriaValid, selectTravelSearchCriteria, selectTravelSearchResults} from "@/lib/selectors";
+import {useAppDispatch, useAppSelector} from "@/lib/hooks";
 import TravelChoice from "@tripolite/common/models/travel-choice";
 import TRAVEL_METHODS_ICONS from "@/types/travel-method-icons";
 import {IconSvgProps} from "@/types";
 import {Spinner} from "@nextui-org/spinner";
-import {Button} from "@nextui-org/button";
-import {ScrollShadow} from "@nextui-org/scroll-shadow";
 import {Listbox, ListboxItem} from "@nextui-org/listbox";
+import {setSearchResults} from "@/lib/features/travelRoutes/travelsSearchSlice";
+import {useRouter} from "next/navigation";
 
 export default function SearchPage() {
     const [isLoadingResults, showLoadingResults] = useState(false);
@@ -20,15 +19,18 @@ export default function SearchPage() {
         = useState('There are no available travel choices for your search');
     const [matchIcon, setMatchIcon]
         = useState<React.FC<IconSvgProps>>();
-    const [searchResults, setSearchResults]
-        = useState<TravelChoice[] | undefined>(undefined);
 
     const searchCriteria = useAppSelector(selectTravelSearchCriteria);
+    const isSearchCriteriaValid = useAppSelector(isTravelSearchCriteriaValid);
+    const searchResults = useAppSelector(selectTravelSearchResults);
+
+    const dispatch = useAppDispatch();
+    const router = useRouter();
 
     useEffect(() => {
         showLoadingResults(true);
 
-        if (searchCriteria) {
+        if (isSearchCriteriaValid) {
             if (searchCriteria.type) {
                 setMatchIcon(TRAVEL_METHODS_ICONS[searchCriteria.type]);
             }
@@ -37,7 +39,7 @@ export default function SearchPage() {
                 .then(results => results.map(r => new TravelChoice(r.paths, searchCriteria)))
                 .then(results => {
                     showLoadingResults(false);
-                    setSearchResults(results);
+                    dispatch(setSearchResults(results));
 
                     if (results.length > 0) {
                         setStatusMessage(`There are ${results.length} travel choices for you to choose from`);
@@ -57,7 +59,8 @@ export default function SearchPage() {
             </p>
             {isLoadingResults && <Spinner label="Searching..."/>}
             {!isLoadingResults && searchResults && searchResults.length > 0 &&
-                <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+                <div
+                    className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
                     <Listbox
                         classNames={{
                             base: "max-w-xs",
@@ -70,18 +73,18 @@ export default function SearchPage() {
                         variant="flat"
                     >
 
-                    {searchResults.map((travelChoice: TravelChoice, index: number) => (
-                        <ListboxItem key={index}
-                                     textValue={`${travelChoice.paths.length} trips, ${travelChoice.satisfactionRatio * 100}% of satisfaction`}>
-                            <div className="flex gap-2 items-center">
-                                <div className="flex flex-col">
+                        {searchResults.map((travelChoice: TravelChoice, index: number) => (
+                            <ListboxItem key={index}
+                                         textValue={`${travelChoice.paths.length} trips, ${travelChoice.satisfactionRatio * 100}% of satisfaction`}>
+                                <div className="flex gap-2 items-center">
+                                    <div className="flex flex-col">
                                     <span
                                         className="text-small">{`${travelChoice.paths.length} trips, ${travelChoice.satisfactionRatio}% of satisfaction ratio`}</span>
-                                    <span className="text-tiny text-default-400">${travelChoice.price}</span>
+                                        <span className="text-tiny text-default-400">${travelChoice.price}</span>
+                                    </div>
                                 </div>
-                            </div>
-                        </ListboxItem>
-                    ))}
+                            </ListboxItem>
+                        ))}
                     </Listbox>
                 </div>
             }
